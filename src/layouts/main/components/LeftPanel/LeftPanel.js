fb68f33041c10cbc062c51fc0da59bc7cdf6738e
rarcz29 <rafalcz29@gmail.com>
2021-03-30 16:00:56 +0200

    Left panel content positioning and scrolling

--- src/layouts/main/components/LeftPanel/LeftPanel.js
+++ src/layouts/main/components/LeftPanel/LeftPanel.js
@@ -10,102 +10,112 @@ import Button from 'components/common/Button';
 import SearchInput from 'components/common/SearchInput';
 import React from 'react';
 import { TreeItem, TreeView } from '../../../../components/common/tree';
-import { ButtonsContainer, Container, TreeViewContainer } from './style';
+import {
+    ButtonsContainer,
+    Container,
+    ContentContainer,
+    TopInputsContainer,
+    TreeViewContainer,
+} from './style';
 
 const LeftPanel = ({ className }) => {
     return (
         <Container className={className}>
-            <SearchInput width="100%" height="45px" />
-            <ButtonsContainer>
-                <Button
-                    width="48%"
-                    height="40px"
-                    variant="contained"
-                    startIcon={<FontAwesomeIcon icon={faPlusSquare} />}
-                >
-                    New
-                </Button>
-                <Button
-                    width="48%"
-                    height="40px"
-                    variant="outlined"
-                    startIcon={<FontAwesomeIcon icon={faFilter} />}
-                >
-                    Filter
-                </Button>
-            </ButtonsContainer>
-            <TreeViewContainer>
-                <TreeView
-                    defaultCollapseIcon={<FontAwesomeIcon icon={faCaretDown} />}
-                    defaultExpandIcon={<FontAwesomeIcon icon={faCaretRight} />}
-                    multiSelect
-                >
-                    <TreeItem
-                        nodeId="1"
-                        label={
-                            <div>
-                                <FontAwesomeIcon icon={faGithub} />
-                                <span>GitHub</span>
-                            </div>
-                        }
+            <TopInputsContainer>
+                <SearchInput width="100%" height="45px" />
+                <ButtonsContainer>
+                    <Button
+                        width="48%"
+                        height="40px"
+                        variant="contained"
+                        startIcon={<FontAwesomeIcon icon={faPlusSquare} />}
                     >
-                        <TreeItem nodeId="2" label="Calendar" />
-                        <TreeItem nodeId="3" label="Chrome" />
-                        <TreeItem nodeId="4" label="Webstorm" />
-                    </TreeItem>
-                    <TreeItem nodeId="5" label="Bitbucket">
-                        <TreeItem nodeId="6" label="Material-UI">
-                            <TreeItem nodeId="7" label="src">
-                                <TreeItem nodeId="8" label="index.js" />
-                                <TreeItem nodeId="9" label="tree-view.js" />
-                            </TreeItem>
-                        </TreeItem>
-                    </TreeItem>
-                    <TreeItem nodeId="10" label="GitLab">
-                        <TreeItem nodeId="11" label="Material-UI">
-                            <TreeItem nodeId="12" label="src">
-                                <TreeItem nodeId="13" label="index.js" />
-                                <TreeItem nodeId="14" label="tree-view.js" />
-                            </TreeItem>
-                        </TreeItem>
-                    </TreeItem>
-                </TreeView>
-                {/* <TreeView
-                    defaultCollapseIcon={<FontAwesomeIcon icon={faCaretDown} />}
-                    defaultExpandIcon={<FontAwesomeIcon icon={faCaretRight} />}
-                    multiSelect
-                >
-                    <TreeItem
-                        nodeId="1"
-                        label={
-                            <div>
-                                <FontAwesomeIcon icon={faGithub} />
-                                <span>GitHub</span>
-                            </div>
-                        }
+                        New
+                    </Button>
+                    <Button
+                        width="48%"
+                        height="40px"
+                        variant="outlined"
+                        startIcon={<FontAwesomeIcon icon={faFilter} />}
                     >
-                        <TreeItem nodeId="2" label="Calendar" />
-                        <TreeItem nodeId="3" label="Chrome" />
-                        <TreeItem nodeId="4" label="Webstorm" />
-                    </TreeItem>
-                    <TreeItem nodeId="5" label="Bitbucket">
-                        <TreeItem nodeId="6" label="Material-UI">
-                            <TreeItem nodeId="7" label="src">
-                                <TreeItem nodeId="8" label="index.js" />
-                                <TreeItem nodeId="9" label="tree-view.js" />
+                        Filter
+                    </Button>
+                </ButtonsContainer>
+            </TopInputsContainer>
+            <ContentContainer>
+                <TreeViewContainer>
+                    <TreeView
+                        defaultCollapseIcon={
+                            <FontAwesomeIcon icon={faCaretDown} />
+                        }
+                        defaultExpandIcon={
+                            <FontAwesomeIcon icon={faCaretRight} />
+                        }
+                        multiSelect
+                    >
+                        <TreeItem
+                            nodeId="1"
+                            label={
+                                <div>
+                                    <FontAwesomeIcon icon={faGithub} />
+                                    <span>GitHub</span>
+                                </div>
+                            }
+                        >
+                            <TreeItem nodeId="2" label="Calendar" />
+                            <TreeItem nodeId="3" label="Chrome" />
+                            <TreeItem nodeId="4" label="Webstorm" />
+                        </TreeItem>
+                        <TreeItem nodeId="5" label="Bitbucket">
+                            <TreeItem nodeId="6" label="Material-UI">
+                                <TreeItem nodeId="7" label="src">
+                                    <TreeItem nodeId="8" label="index.js" />
+                                    <TreeItem nodeId="9" label="tree-view.js" />
+                                </TreeItem>
                             </TreeItem>
                         </TreeItem>
-                    </TreeItem>
-                    <TreeItem nodeId="5" label="GitLab">
-                        <TreeItem nodeId="6" label="Material-UI">
-                            <TreeItem nodeId="7" label="src">
-                                <TreeItem nodeId="8" label="index.js" />
-                                <TreeItem nodeId="9" label="tree-view.js" />
+                        <TreeItem nodeId="10" label="GitLab">
+                            <TreeItem nodeId="11" label="Material-UI">
+                                <TreeItem nodeId="12" label="src">
+                                    <TreeItem nodeId="13" label="index.js" />
+                                    <TreeItem
+                                        nodeId="14"
+                                        label="tree-view.js"
+                                    />
+                                </TreeItem>
+                                <TreeItem nodeId="15" label="src">
+                                    <TreeItem nodeId="16" label="index.js" />
+                                    <TreeItem
+                                        nodeId="17"
+                                        label="tree-view.js"
+                                    />
+                                </TreeItem>
+                                <TreeItem nodeId="12" label="src">
+                                    <TreeItem nodeId="13" label="index.js" />
+                                    <TreeItem
+                                        nodeId="14"
+                                        label="tree-view.js"
+                                    />
+                                </TreeItem>
+                                <TreeItem nodeId="12" label="src">
+                                    <TreeItem nodeId="13" label="index.js" />
+                                    <TreeItem
+                                        nodeId="14"
+                                        label="tree-view.js"
+                                    />
+                                </TreeItem>
+                                <TreeItem nodeId="12" label="src">
+                                    <TreeItem nodeId="13" label="index.js" />
+                                    <TreeItem
+                                        nodeId="14"
+                                        label="tree-view.js"
+                                    />
+                                </TreeItem>
                             </TreeItem>
                         </TreeItem>
-                    </TreeItem>
-                </TreeView> */}
-            </TreeViewContainer>
+                    </TreeView>
+                </TreeViewContainer>
+            </ContentContainer>
         </Container>
     );
 };
--- src/layouts/main/components/LeftPanel/style.js
+++ src/layouts/main/components/LeftPanel/style.js
@@ -1,9 +1,10 @@
+import { TOP_BAR_HEIGHT } from 'constants/sizes';
 import styled from 'styled-components';
 import COLORS from 'themes/colors';
 
 export const Container = styled.div`
     width: 350px;
-    height: 100%;
+    height: calc(100vh - ${TOP_BAR_HEIGHT + 2}px);
     background-color: ${COLORS.BACKGROUND.darker};
     //padding: 1rem;
 `;
@@ -14,6 +15,16 @@ export const ButtonsContainer = styled.div`
     justify-content: space-between;
 `;
 
+export const TopInputsContainer = styled.div`
+    margin: 1rem;
+`;
+
+export const ContentContainer = styled.div`
+    margin: 2rem 0 0 2rem;
+    height: calc(100% - 85px - 4rem);
+    overflow-y: auto;
+`;
+
 export const TreeViewContainer = styled.div`
-    margin: 2rem 1rem 0 1rem;
+    margin-right: 2rem;
 `;
