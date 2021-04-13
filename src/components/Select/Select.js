7d5ac744058b4071c6f39bcd5e46006113bb476b
rarcz29 <rafalcz29@gmail.com>
2021-04-13 18:55:25 +0200

    Create a typescript app

--- src/components/Select/Select.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyledSelect } from './styles';
-
-const Select = (props) => {
-    return <StyledSelect {...props} />;
-};
-
-export default Select;
--- src/components/common/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyledButton } from './styles';
-
-const Button = (props) => {
-    return <StyledButton {...props} />;
-};
-
-export default Button;
