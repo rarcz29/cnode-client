6fc85df32cc88d9a922d4cbb7585ebe0f57184c1
rarcz29 <rafalcz29@gmail.com>
2021-06-19 23:54:12 +0200

    Update the side nav

--- src/components/layouts/HomeLayout/components/SideNav/SideNav.tsx
+++ src/components/layouts/HomeLayout/components/SideNav/SideNav.tsx
@@ -1,20 +1,52 @@
-import { faSlack } from '@fortawesome/free-brands-svg-icons';
+import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import styled from 'styled-components';
 import NavItem from '../NavItem';
+import { Aside } from './style';
+
+type Item = {
+  to: string;
+  icon: IconDefinition;
+  text: string;
+};
+
+const items: Item[] = [
+  {
+    to: '#',
+    icon: faHome,
+    text: 'Home',
+  },
+  {
+    to: '#',
+    icon: faHome,
+    text: 'Repositories',
+  },
+  {
+    to: '#',
+    icon: faHome,
+    text: 'Contacts',
+  },
+  {
+    to: '#',
+    icon: faHome,
+    text: 'Teams',
+  },
+];
 
 type SideNavProps = {};
 
-const Aside = styled.aside`
-  width: 248px;
-`;
-
 const SideNav: React.FC<SideNavProps> = () => (
   <Aside>
-    <NavItem to="#" icon={<FontAwesomeIcon icon={faSlack} />}>
-      Item
-    </NavItem>
+    {items.map((item) => (
+      <NavItem
+        key={item.text}
+        to={item.to}
+        icon={<FontAwesomeIcon icon={item.icon} />}
+      >
+        {item.text}
+      </NavItem>
+    ))}
   </Aside>
 );
 
